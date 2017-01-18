a85619508738ce012fefd36950730425462a6401
Aliza Koresh <lizzie.kor@gmail.com>
2017-01-18 15:13:49 +0200

    Search button added

--- app/src/main/java/finals/shotefplus/activities/MainActivity.java
+++ app/src/main/java/finals/shotefplus/activities/MainActivity.java
@@ -1,10 +1,17 @@
 package finals.shotefplus.activities;
 
+import android.app.SearchManager;
+import android.content.Context;
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
+
 import android.os.Bundle;
+
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.View;
 import android.widget.Button;
+import android.widget.SearchView;
 
 import finals.shotefplus.R;
 
@@ -86,5 +93,40 @@ public class MainActivity extends AppCompatActivity {
 
 
     }
+
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu, menu);
+
+        // Associate searchable configuration with the SearchView
+        SearchManager searchManager = (SearchManager) getSystemService(Context.SEARCH_SERVICE);
+        SearchView searchView = (SearchView) menu.findItem(R.id.search).getActionView();
+        searchView.setSearchableInfo(searchManager.getSearchableInfo(getComponentName()));
+
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                //Log.e("onQueryTextChange", "called");
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+
+                startActivity(new Intent(MainActivity.this,SearchResultsActivity.class));
+                // Do your task here
+
+                return false;
+            }
+
+        });
+
+        return true;//super.onCreateOptionsMenu(menu);
+    }
+
 
 }
